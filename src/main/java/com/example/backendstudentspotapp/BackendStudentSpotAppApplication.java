12348caecd188e286c4c0c5c4b596fd4d7b1e52e
niquolic <nicolas.debruycker22@gmail.com>
2023-03-03 00:16:54 +0100

    Initial commit

---
+++ src/main/java/com/example/backendstudentspotapp/BackendStudentSpotAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.backendstudentspotapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendStudentSpotAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendStudentSpotAppApplication.class, args);
+	}
+
+}
