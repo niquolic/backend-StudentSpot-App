02ea50c992f29e9cdae1c5c51723df86c1557473
niquolic <nicolas.debruycker22@gmail.com>
2023-03-11 15:28:39 +0100

    get ecole

--- src/main/java/com/example/backendstudentspotapp/controller/EcoleController.java
+++ src/main/java/com/example/backendstudentspotapp/controller/EcoleController.java
@@ -3,7 +3,6 @@ package com.example.backendstudentspotapp.controller;
 import com.example.backendstudentspotapp.entities.EcoleEntity;
 import com.example.backendstudentspotapp.service.ServiceEcole;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,7 +17,7 @@ public class EcoleController {
     ServiceEcole serviceEcole;
 
     @GetMapping("/getEcoleByIdGroupe/{id_groupe}")
-    public List<EcoleEntity> getEcoleByIdGroupe(@PathVariable Long id_groupe) {
+    public List<EcoleEntity> getEcoleByIdGroupe(@PathVariable long id_groupe) {
 
 
         // -- Test
